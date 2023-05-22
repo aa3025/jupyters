b55f07329f2a7b9a3d0cf9f0b422bc80968d285d
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2023-05-22 12:41:09 +0100

    Update 01_build_image.sh

--- 01_build_image.sh
+++ 01_build_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # build image and start the container
 
-docker build . -t my/image:last | tee build.log
+docker build . -t my/image:pip | tee build.log
 
 # get id of the built image
 image_id=$(tail -n1 build.log | cut -d' ' -f3)
 
 # optionally upload to your dockerhub account
 # docker logn
-# docker push my/image:last
+# docker push my/image:pip
 
 sh ./02_startdocker.sh $image_id
